11ccc76da1daf96202177ffaef31ee414af9cbb7
theobontempelli <theobtp@gmail.com>
2016-02-11 19:43:00 +0100

    Qq changements

--- src/hashcode/Emplacement.java
+++ src/hashcode/Emplacement.java
@@ -1,5 +1,11 @@
 package hashcode;
 
 public abstract class Emplacement {
-	
+	protected Coordonnees location;
+	public Coordonnees getLocation() {
+		return location;
+	}
+	public void setLocation(Coordonnees location) {
+		this.location = location;
+	}
 }
--- src/hashcode/Entrepot.java
+++ src/hashcode/Entrepot.java
@@ -1,5 +1,19 @@
 package hashcode;
 
-public class Entrepot extends Emplacement {
+import java.util.ArrayList;
 
+public class Entrepot extends Emplacement {
+	private ArrayList<Produit> produits_disponibles;
+	public Entrepot(int x, int y){
+		produits_disponibles=new ArrayList<Produit>();
+		location=new Coordonnees(x,y);
+	}
+	public ArrayList<Produit> getProduits_disponibles() {
+		return produits_disponibles;
+	}
+	public void setProduits_disponibles(ArrayList<Produit> produits_disponibles) {
+		this.produits_disponibles = produits_disponibles;
+	}
+
+	
 }
