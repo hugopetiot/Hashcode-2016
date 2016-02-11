76ef35240c14acc46a1a950ed0c33919a363d624
Djczq <jeromedjczq@gmail.com>
2016-02-11 18:19:09 +0100

    changement main

--- src/hashcode/Main.java
+++ src/hashcode/Main.java
@@ -1,48 +1,75 @@
 package hashcode;
+
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
+
 public class Main {
+
 	static Scanner in = new Scanner(System.in);
-	
-	
+
 	public static void main(String args[]){
-		Datacenter d= loadData(new File("dc.in.txt"));
-		System.out.println(d.toString());
-		d.rangement1();
-		System.out.println(d.score());
-		d.etat();
-	}
-	
-	public static Datacenter loadData(File f){
-		Datacenter datac = null;		
-		int rangees, emplacements, emplacementsIndisponibles, groupes, serveurs;
-		int i;
+		long cdeb = System.currentTimeMillis();
+
 		try {
-			FileInputStream fis = new FileInputStream(f);
-			in = new Scanner(fis);
-			//2rangeesde5emplacements,1emplacementindisponible,2groupeset5serveurs.
-			rangees = in.nextInt();
-			emplacements= in.nextInt(); 
-			emplacementsIndisponibles= in.nextInt(); 
-			groupes= in.nextInt(); 
-			serveurs= in.nextInt();
-			System.out.println(rangees+"  "+emplacements);
-			datac = new Datacenter(rangees, emplacements, emplacementsIndisponibles, groupes, serveurs);
-			
-			for(  i = 0 ; i<emplacementsIndisponibles; i++)
-				datac.ajoutIndisponible(in.nextInt(), in.nextInt());
-			
-			for( i = 0 ; i<serveurs; i++)
-				datac.ajoutListeServeur(in.nextInt(), in.nextInt());
-			
-			datac.etat();
-		} catch (FileNotFoundException e) {
+			String rendu="";
+			FileWriter fw= new FileWriter(new File("logo.txt"));
+			Locale.setDefault(Locale.ENGLISH);
+			int nbcom=0;
+			Dessin dessin = new Dessin(new File("logo.in"));
+			//Dessin dessin = new Dessin(new File("learn_and_teach.in"));
+			//Dessin dessin = new Dessin(new File("right_angle.in"));
+
+			int maxl, maxc, maxsq,max;
+
+			while(dessin.getHashtag()!=0){
+				nbcom++;
+				scLig=dessin.ScoreLigne();
+				scCol=dessin.ScoreColonne();
+				sq=dessin.ScoreCarre();
+				maxl = scLig.getScore();
+				maxc= scCol.getScore();
+				maxsq= sq.getScore();
+
+				if(maxl < maxc){
+					max = maxc;
+				}else{
+					max = maxl;
+				}
+
+				if(max < maxsq){
+					dessin.changeSquarre(sq);
+					rendu+=sq.toString();
+				}else{
+					if(maxl<maxc){
+
+						dessin.changeLine(scCol);
+						rendu+=scCol.toString();
+					}else{
+						dessin.changeLine(scLig);	
+						rendu+=scLig.toString();
+					}
+				}
+
+				//System.out.println("carre "+sq.getScore());
+				//System.out.println("# "+dessin.getHashtag());
+			}
+			//System.out.println(dessin.toString());
+			System.out.println(nbcom);
+			fw.write(nbcom+"\n");
+			fw.write(rendu);
+		//	fw.write(dessin.toString());
+			fw.close();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return datac;
-	}
+		
+		long c = System.currentTimeMillis() - cdeb;
+		System.out.println(c);
+	}	
 }
+
