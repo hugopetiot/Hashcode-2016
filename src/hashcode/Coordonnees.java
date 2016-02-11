475cec73d6cab61300649c83d354ed4c12548cb6
theobontempelli <theobtp@gmail.com>
2016-02-11 19:26:08 +0100

    Distance

--- src/hashcode/Coordonnees.java
+++ src/hashcode/Coordonnees.java
@@ -9,4 +9,25 @@ public class Coordonnees {
 		this.y = y;
 	}
 	
+	public int distance(Coordonnees c){
+		double distance = Math.sqrt((x-c.getX())*(x-c.getX())+(y-getY())*(y-getY()));
+		return (int) Math.ceil(distance);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
 }
