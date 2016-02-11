d8dc3c4fc2d75b7a145151364b690e387b8c5889
theobontempelli <theobtp@gmail.com>
2016-02-11 19:38:02 +0100

    Commande + Produit

--- src/hashcode/Commande.java
+++ src/hashcode/Commande.java
@@ -1,5 +1,25 @@
 package hashcode;
 
-public class Commande extends Emplacement {
+import java.util.ArrayList;
 
+public class Commande extends Emplacement {
+	private ArrayList<Produit> produits;
+	private Coordonnees location;
+	public Commande(int x, int y){
+		produits= new ArrayList<Produit>();
+		location=new Coordonnees(x,y);
+	}
+	public ArrayList<Produit> getProduits() {
+		return produits;
+	}
+	public void setProduits(ArrayList<Produit> produits) {
+		this.produits = produits;
+	}
+	public Coordonnees getLocation() {
+		return location;
+	}
+	public void setLocation(Coordonnees location) {
+		this.location = location;
+	}
+	
 }
--- src/hashcode/Produit.java
+++ src/hashcode/Produit.java
@@ -2,13 +2,23 @@ package hashcode;
 
 public class Produit {
 	private int id;
-
+	private int weight;
+	public Produit(int i,int w){
+		id=i;
+		weight=w;
+	}
 	public int getId() {
 		return id;
 	}
 
 	public void setId(int id) {
 		this.id = id;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
 	}
 	
 	
