b13c33ff4691492c21da1c5f406fae0ea3784cf2
Hugo <hugo.petiot@gmail.com>
2016-02-11 19:31:37 +0100

    Classe drone

--- src/hashcode/Drone.java
+++ src/hashcode/Drone.java
@@ -1,5 +1,72 @@
 package hashcode;
 
 public class Drone {
-	int tailleMax;
+	private int capaciteMax, charge;
+	private Coordonnees position, destination;
+	private int tempsRestant;
+	
+	public Drone(int capa, Coordonnees posInit){
+		capaciteMax = capa;
+		position = posInit;
+		destination = new Coordonnees(0, 0);
+		charge = 0;
+		tempsRestant = 0;
+	}
+	
+	
+	public int getCapaciteMax() {
+		return capaciteMax;
+	}
+	public void setCapaciteMax(int tailleMax) {
+		this.capaciteMax = tailleMax;
+	}
+	public Coordonnees getPosition() {
+		return position;
+	}
+	public void setPosition(Coordonnees position) {
+		this.position = position;
+	}
+
+
+
+
+	public int getCharge() {
+		return charge;
+	}
+
+
+
+
+	public void setCharge(int charge) {
+		this.charge = charge;
+	}
+
+
+
+
+	public Coordonnees getDestination() {
+		return destination;
+	}
+
+
+
+
+	public void setDestination(Coordonnees destination) {
+		this.destination = destination;
+	}
+
+
+
+
+	public int getTempsRestant() {
+		return tempsRestant;
+	}
+
+
+
+
+	public void setTempsRestant(int tempsRestant) {
+		this.tempsRestant = tempsRestant;
+	}
+	
 }
