78cd0abe3f558df10214e58d8d5daf665cb3a9ec
Djczq <jeromedjczq@gmail.com>
2016-02-11 18:24:07 +0100

    re

--- src/hashcode/Main.java
+++ src/hashcode/Main.java
@@ -19,55 +19,24 @@ public class Main {
 			FileWriter fw= new FileWriter(new File("logo.txt"));
 			Locale.setDefault(Locale.ENGLISH);
 			int nbcom=0;
-			Dessin dessin = new Dessin(new File("logo.in"));
 			//Dessin dessin = new Dessin(new File("learn_and_teach.in"));
 			//Dessin dessin = new Dessin(new File("right_angle.in"));
 
-			int maxl, maxc, maxsq,max;
 
-			while(dessin.getHashtag()!=0){
-				nbcom++;
-				scLig=dessin.ScoreLigne();
-				scCol=dessin.ScoreColonne();
-				sq=dessin.ScoreCarre();
-				maxl = scLig.getScore();
-				maxc= scCol.getScore();
-				maxsq= sq.getScore();
 
-				if(maxl < maxc){
-					max = maxc;
-				}else{
-					max = maxl;
-				}
-
-				if(max < maxsq){
-					dessin.changeSquarre(sq);
-					rendu+=sq.toString();
-				}else{
-					if(maxl<maxc){
-
-						dessin.changeLine(scCol);
-						rendu+=scCol.toString();
-					}else{
-						dessin.changeLine(scLig);	
-						rendu+=scLig.toString();
-					}
-				}
-
-				//System.out.println("carre "+sq.getScore());
-				//System.out.println("# "+dessin.getHashtag());
-			}
+			//System.out.println("carre "+sq.getScore());
+			//System.out.println("# "+dessin.getHashtag());
 			//System.out.println(dessin.toString());
 			System.out.println(nbcom);
 			fw.write(nbcom+"\n");
 			fw.write(rendu);
-		//	fw.write(dessin.toString());
+			//	fw.write(dessin.toString());
 			fw.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		long c = System.currentTimeMillis() - cdeb;
 		System.out.println(c);
 	}	
