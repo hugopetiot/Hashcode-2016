f613a02441e6f62b459da93ab6337d7cb0c015aa
theobontempelli <theobtp@gmail.com>
2016-02-11 19:32:30 +0100

    Classe Produit

--- src/hashcode/Emplacement.java
+++ src/hashcode/Emplacement.java
@@ -1,4 +1,5 @@
 package hashcode;
 
 public abstract class Emplacement {
+	
 }
---
+++ src/hashcode/Produit.java
@@ -0,0 +1,16 @@
+package hashcode;
+
+public class Produit {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+
+}
